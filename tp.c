c0bd1658deac31fbc5f38e0256d6974ad3e08eb9
ahmed ksentini <ks.ahmed.94@gmail.com>
2015-12-15 20:15:27 +0100

    Update tp.c

--- tp.c
+++ tp.c
@@ -1,56 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct liste
-{
-	char *s ;
-	struct liste *next ;
+struct liste {
+    char *s;
+    struct liste *next;
 };
 typedef struct liste LISTE;
 
-  // fonction affiche toute la liste
-  void affiche_tliste(LISTE *first)
- {
- 	LISTE *p ;
- 	for (p=first ; p ; p=p->next)
- 	 printf ("%s",p->s) ;
+// fonction affiche toute la liste
+void affiche_tliste(LISTE *first) {
+    LISTE *p;
+    for (p = first; p; p = p->next)
+        printf("%s", p->s);
 
- }
+}
 
 //fonction insertion
- void insert_tliste(LISTE**first,char ligne)
- {
-   LISTE *p ;
-p =(LISTE*)malloc(sizeof(LISTE));
-p->s =(char*)malloc(strlen(*ligne)+1) ;
-strcpy(p->s,ligne);
- p->next=*first ;
- *first = p ;
+void insert_tliste(LISTE **first, char *ligne) {
+    LISTE *p;
+    p = (LISTE *) malloc(sizeof(LISTE));
+    p->s = (char *) malloc(sizeof(char) * (strlen(ligne) + 1));
+    strcpy(p->s, ligne);
+    p->next = *first;
+    *first = p;
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) {
+    FILE *fp;
 
-{
-	FILE *fp ;
-
-LISTE *first ;
-char *ligne [200] ;
-	if (!(fp=fopen("ahmed.txt","r")))
-	exit(0) ;
-	first=NULL ;
+    LISTE *first;
+    char ligne[200];
+    if (!(fp = fopen("ahmed.txt", "r")))
+        exit(0);
+    first = NULL;
 
     // creation de la liste
-	while (fgets(*ligne,200,fp))
-	 	{
-	 		insert_tliste(&first,ligne) ;
+    while (fgets(ligne, 200, fp)) {
+        insert_tliste(&first, ligne);
 
-	 	}
+    }
     // fin de la creation du liste .
 
- // affiche toutes la liste la liste
-  affiche_tliste(first) ;
- 
+    // affiche toutes la liste la liste
+    affiche_tliste(first);
 
-  return 0;
+
+    return 0;
 }
