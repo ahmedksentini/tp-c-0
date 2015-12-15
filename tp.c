537c2d22bf590929ac0cf2df11ded2f95c4893c0
ahmed ksentini <ks.ahmed.94@gmail.com>
2015-12-15 23:52:14 +0100

    Update tp.c

--- tp.c
+++ tp.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct liste {
@@ -24,19 +24,40 @@ void insert_tliste(LISTE **first, char *ligne) {
     LISTE *p;
     p = (LISTE *) malloc(sizeof(LISTE));
     p->s = (char *) malloc(sizeof(char) * (strlen(ligne) + 1));
-    strcpy(p->s, ligne);
+    strcpy(p->s,ligne);
     p->next = *first;
     *first = p;
 }
 
+//fonction recherche qui recherche ou se trouve une chaine
 
+void recherche_chaine( LISTE * first, char* s_chaine)
+{
+    LISTE * current;
+    current =  first;
+    while (current != NULL)
+    {
+        if ( strstr ((current->s), s_chaine) == NULL)
+        current = current ->next;
+        else
+        {
+             printf("%s",current->s);
+            current = current->next;
+        }
+    }
+}
 int main(int argc, char *argv[]) {
     FILE *fp;
 
     LISTE *first;
     char ligne[200];
+     char sous_chaine [20], s_chaine [20] ;
     if (!(fp = fopen("ahmed.txt", "r")))
+    {
+
+
         exit(0);
+    }
     first = NULL;
 
     // creation de la liste
@@ -49,6 +70,9 @@ int main(int argc, char *argv[]) {
     // affiche toutes la liste la liste
     affiche_tliste(first);
 
-
+printf ("\n\n \a  donner la chaine a chercher :");
+    scanf ("%s", &sous_chaine[0]);
+    recherche_chaine(first, sous_chaine);
     return 0;
 }
+
